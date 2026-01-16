2284fba72ff4ef14038342cd507b043d4406315b
anaresgalla <anaresgalla@gmail.com>
2026-01-16 16:10:02 -0300

    Adiciona validações de dados de cadastro e endereço obrigatórios

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         System.out.println(dados);
     }
 }
--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -1,7 +1,25 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
+import org.hibernate.validator.constraints.br.CPF;
 
-public record DadosCadastroPaciente(String nome, String cpf, String email,
-                                    String telefone, DadosEndereco endereco) {
+public record DadosCadastroPaciente(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @CPF
+        String cpf,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank @Pattern(regexp = "\\d{10,14}") //tem de 10 a 14 digitos
+        String telefone,
+        @NotNull
+        @Valid // para validar outro obj que é o dto do endrereço
+        DadosEndereco endereco) {
 }
