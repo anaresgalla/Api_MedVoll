5c46d4fcb33c46ce22ce86266de92f40f939a47a
anaresgalla <anaresgalla@gmail.com>
2026-01-20 12:19:42 -0300

    Ajusta paginação e ordenação da listagem de médicos cadastrados

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -7,9 +7,11 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @RestController
@@ -25,8 +27,10 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream()
-                .map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar
+            (@PageableDefault(size = 10, sort={"nome"}) Pageable paginacao){
+        //esses parametros podem ser sobrescritos caso sejam passados na url
+        return repository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 }
