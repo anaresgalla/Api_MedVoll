3b6bc3a89512d908df62fc4ab6f7c5db21f2dd72
anaresgalla <anaresgalla@gmail.com>
2026-01-26 10:58:19 -0300

    Cria classe Paciente

--- src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosCadastroPaciente.java
@@ -6,13 +6,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
-import org.hibernate.validator.constraints.br.CPF;
 
 public record DadosCadastroPaciente(
         @NotBlank
         String nome,
         @NotBlank
-        @CPF
+        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
         String cpf,
         @NotBlank
         @Email
