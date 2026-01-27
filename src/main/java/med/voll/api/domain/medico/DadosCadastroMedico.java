c12ef86c9b39aab289ded9d81b98361b822a5526
anaresgalla <anaresgalla@gmail.com>
2026-01-27 11:12:12 -0300

    Implementa Tratamento de erros

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -1,23 +1,25 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.domain.endereco.DadosEndereco;
 
-public record DadosCadastroPaciente(
+public record DadosCadastroMedico(
         @NotBlank
         String nome,
-        @NotBlank
-        @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")
-        String cpf,
         @NotBlank
         @Email
         String email,
         @NotBlank
         String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //são de 4 a 6 digitos
+        String crm,
+        @NotNull
+        Especialidade especialidade,
         @NotNull
         @Valid // para validar outro obj que é o dto do endrereço
         DadosEndereco endereco) {
